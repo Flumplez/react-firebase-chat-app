d810a7b0f7aae495216f2fa080123f5a43b15e3b
Austin Davison <austindagamingpro@gmail.com>
2024-10-07 21:19:43 +0100

    Added google user auth and cookies to manage it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { useState } from 'react'
 import './App.css';
+import './styles/Chat.css'
+import { Auth } from './components/Auth';
+
+import Cookies from 'universal-cookie'
+const cookies = new Cookies();
 
 function App() {
+
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState("")
+
+  if(!isAuth) {
+    return (
+      <div>
+        <Auth/>
+      </div>
+    );
+  }
+
   return (
-    <div>Hello World!</div>
-  );
+  <div>
+    {room ? (
+      <div> Chat</div>
+    ) : (
+      <div className='room'>
+        <label>Enter Room Name:</label>
+        <input></input>
+        <button>Enter Chat</button>
+      </div>
+    )}
+    </div>
+
+)
 }
 
 export default App;
---
+++ src/components/Auth.js
@@ -0,0 +1,27 @@
+import '../styles/Auth.css'
+import { auth, provider } from '../firebase-config'
+import { signInWithPopup } from 'firebase/auth'
+
+import Cookies from 'universal-cookie'
+const cookies = new Cookies();
+
+export const Auth = () => {
+
+    const signInWithGoogle = async () => {
+
+        try {
+            const result = await signInWithPopup(auth, provider);
+            cookies.set("auth-token", result.user.refreshToken);
+        } catch(err) {
+            console.error(err)
+        }
+    };
+
+    return (
+        <div className="auth">
+            <p>Sign In With Google to Continue</p>
+            <button onClick={signInWithGoogle}>Sign in With Google</button>
+        </div>
+
+    );
+}
